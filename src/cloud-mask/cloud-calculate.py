f0730f0d60e98dc0191e646a80683702f2d63430
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2018-08-02 11:08:25 -0300

    Cloud Calculate and Cloud Calculate Notebook

---
+++ src/cloud-mask/cloud-calculate.py
@@ -0,0 +1,38 @@
+from osgeo import gdal
+import numpy as np
+
+try:
+	nuvem1 = gdal.Open("data/LC08_L1TP_221067_20170926_20171013_01_T1_CLD.TIF")
+	nuvem2 = gdal.Open("data/221_067_2017-01-13_nuvem.tif")
+	print ("Arquivos aberto com sucesso!")
+except:
+	print("Erro na abertura dos arquivo!")
+	exit()
+
+mascara1 = nuvem1.GetRasterBand(1).ReadAsArray().astype(np.int8)
+mascara2 = nuvem2.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
+nuvem2 = np.sum(mascara2==1)
+limpa1 = np.sum(mascara1==0)
+
+tamanho_imagem1 = mascara1.shape[0] * mascara1.shape[1]
+tamanho_imagem2 = mascara2.shape[0] * mascara2.shape[1]
+
+cloud_mask = (limpa1 * 100) / tamanho_imagem1
+nuvem = (100-(nuvem2 * 100) / tamanho_imagem2)
+
+print("")
+
+print("Porcentagem Nuvem (Mascara): %.2f" %(100-cloud_mask) )
+print("Porcentagem Nuvem (Exemplo): %.2f" %(100-nuvem) )
+
+
+print("")
+
+print("Porcentagem Não-Nuvem (Mascara): %.2f" %cloud_mask)
+print("Porcentagem Não-Nuvem (Exemplo): %.2f" %nuvem)
+
+print("")
+
+print("Porcentagem de Acerto: %.2f" %(100-((cloud_mask-nuvem)/cloud_mask)*100) )
+
--- src/cloud-mask/cloud-mask.py
+++ src/cloud-mask/cloud-mask.py
@@ -13,7 +13,6 @@ import cv2
 dir = "/home/sansigolo/Documents/git/CAP-240-394/"
 
 try:
-	B6 = gdal.Open(dir+'LC08_L1TP_221067_20170926_20171013_01_T1_B6.TIF')
 	B3 = gdal.Open(dir+'LC08_L1TP_221067_20170113_20170311_01_T1_B3_C.TIF')
 	B4 = gdal.Open(dir+'LC08_L1TP_221067_20170113_20170311_01_T1_B4_C.TIF')
 	B5 = gdal.Open(dir+'LC08_L1TP_221067_20170113_20170311_01_T1_B5_C.TIF')
@@ -65,34 +64,13 @@ plt.imshow(mask, cmap='gray')
 plt.colorbar()
 plt.show()
 
-# set geotransform
-image_size = mask.shape
-print("image_size: "+str(image_size[0])+","+str(image_size[1]))
-nx = image_size[0]
-ny = image_size[1]
-width = B3.RasterXSize
-height = B3.RasterYSize
-print("width, height: "+str(width)+", "+str(height))
-gt = B3.GetGeoTransform()
-print("gt: "+str(gt))
-minx = gt[0]
-miny = gt[3] + width*gt[4] + height*gt[5] 
-maxx = gt[0] + width*gt[1] + height*gt[2]
-maxy = gt[3] 
-print("minx, miny, maxx, maxy: "+str(minx)+", "+str(miny)+", "+str(maxx)+", "+str(maxy))
-xres = (maxx - minx) / float(nx)
-yres = (maxy - miny) / float(ny)
-geotransform = (minx, xres, 0, maxy, 0, -yres)
-
-# create the 3-band raster file
-dst_ds = gdal.GetDriverByName('GTiff').Create('cloud-mask.tif', ny, nx, 1, gdal.GDT_Byte)
-
-dst_ds.SetGeoTransform(geotransform)    # specify coords
-srs = osr.SpatialReference()            # establish encoding
-srs.ImportFromEPSG(4326)                # WGS84 lat/long
-dst_ds.SetProjection(srs.ExportToWkt()) # export coords to file
-dst_ds.GetRasterBand(1).WriteArray(mask)   # write r-band to the raster
-dst_ds.FlushCache()                     # write to disk
-dst_ds = None
-
-
+# Salve
+filename_output = "data/LC08_L1TP_221067_20170926_20171013_01_T1_CLD.TIF"
+geotiff = gdal.GetDriverByName('GTiff')
+dataset_output = geotiff.Create(filename_output, B3.RasterXSize, B3.RasterYSize, 1, gdal.GDT_Float32)
+dataset_output.SetGeoTransform(B3.GetGeoTransform())
+dataset_output.SetProjection(B3.GetProjectionRef())
+dataset_output.GetRasterBand(1).WriteArray(mask)
+dataset_output.FlushCache()
+dataset_output = None
+print ("Arquivos salvo com sucesso!")
