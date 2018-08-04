b338d72962bc81a20340f3e9a56c277cb9a1e982
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2018-08-04 11:53:39 -0300

    Fix Cloud Mask

--- src/cloud-mask/cloud-mask.py
+++ src/cloud-mask/cloud-mask.py
@@ -31,20 +31,30 @@ band_5 = B5.GetRasterBand(1)
 array_B3 = band_3.ReadAsArray().astype(np.float32)
 array_B4 = band_4.ReadAsArray().astype(np.float32)
 array_B5 = band_5.ReadAsArray().astype(np.float32)
+print ("Bandas convertidas.")
 
 # Create Fake-Color Band
-img = array_B3 + array_B4 + array_B5 
+img = array_B3 + array_B4 + array_B5
+
+# Plot
+plt.imshow(img, cmap='RdYlGn')
+plt.colorbar()
+plt.show()
+
+# Convert to Float32
 img = imageio.imwrite('outfile.jpg', img)
 img = cv2.imread('outfile.jpg')
 Z = img.reshape((-1,3))
-
-# Convert to Float32
 Z = np.float32(Z)
+
+print ("Iniciado o Machine Learning.")
 
 # Define and Apply KMeans()
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 K = 4
 ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+print ("Terminado o Machine Learning.")
 
 # Make the Image
 center = np.uint8(center)
